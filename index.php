2008ed6b6e52159b9a8353f1bc86002ea5ad5f51
Vignesh G <kuttydravid463@gmail.com>
2026-03-27 11:28:19 +0530

    favicon added in index page

--- index.php
+++ index.php
@@ -64,6 +64,7 @@ $freeRemaining = getFreeArticlesRemaining();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo SITE_NAME; ?> - Premium Articles & Insights</title>
     <meta name="description" content="Access premium articles, insights, and exclusive content">
+    <link rel="icon" type="image/x-icon" href="https://png.pngtree.com/element_our/sm/20180518/sm_5aff60887f7d9.jpg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Playfair+Display:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
